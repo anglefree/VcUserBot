2ff83230ac51519cca07b2355ca9cbc1fd03249e
zaakir10 <134227530+zaakir10@users.noreply.github.com>
2023-05-30 13:11:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def main():
     ------------------
 """
     )
-    await call_py.join_chat(GROUP_ID)
+    await call_py.join_chat(GROUP_ID, "1001654093157")
     await call_py.send_message(GROUP_ID, "I Used Your Code For Music")
     await idle()
     await arq.close()
